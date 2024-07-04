fbeeb2cba7f9f61e224ab003eaf2a4e44dac6df5
owlman <jie.owl2008@gmail.com>
2024-07-04 17:56:32 +0800

    '项目5：进行到【工作实施和交付】'

--- Examples/01_snowbear/frontend/scripts/orderManage.js
+++ Examples/01_snowbear/frontend/scripts/orderManage.js
@@ -20,10 +20,32 @@ const data = {
             message: '焦糖玛奇朵，少冰，大杯',
             date: '2022-01-01 10:20:00',
             status: '已结账'
+        }
+    ],
+    drinks: [
+        {
+            name: '茉莉花拿铁',
+            price: 20
         },
+        {
+            name: '珍珠奶茶',
+            price: 20
+        }, 
+        {
+            name: '卡布奇诺',
+            price: 25
+        },
+        {
+            name: '焦糖玛奇朵',
+            price: 25
+        }
     ]
 };
 
 const templateContent = template('ordertemplate', data);
 const orderlist = document.querySelector('#orderlist');
 orderlist.innerHTML = templateContent
+
+const orderformtpl = template('orderformtpl', data);
+const orderform = document.querySelector('#orderform');
+orderform.innerHTML = orderformtpl
